e530badcfdb8cec8268965e158297b996eff51e7
Leon Dudlik <leon.dudlik@img.ly>
2022-01-13 15:18:06 +0100

    Add version 10.28.2

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "ImglyKit", 
-        url: "https://releases.photoeditorsdk.com/ios/ImglyKit-10.28.1.xcframework.zip", 
-        checksum: "aed4f7f64501626d8628cb9b3d526fd1e82f1a23c0196b64d2b336f52a5459c1"),
+        url: "https://releases.photoeditorsdk.com/ios/ImglyKit-10.28.2.xcframework.zip", 
+        checksum: "ddd134bdc4220ae164895997e329cc47720c2ceb0c9f61028b16484d8a26384c"),
   ]
 )
