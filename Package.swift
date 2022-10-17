75a4da07bd8812c5a6ec3b414af54c033227564e
Tim Weisseisen <wajsic@gmail.com>
2022-10-17 14:45:40 +0200

    Add version 11.3.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "ImglyKit", 
-        url: "https://releases.photoeditorsdk.com/ios/ImglyKit-11.3.2.xcframework.zip", 
-        checksum: "5db87dd4bb7c486b75cdc9d265487c1b96edbfb71bb02c9c45cf6ef1a6e44f6e"),
+        url: "https://releases.photoeditorsdk.com/ios/ImglyKit-11.3.3.xcframework.zip", 
+        checksum: "e87ce1c67d5b7de854d5a42dd2f2867f4ae19563c93c7a8b686ad7abe02aea12"),
   ]
 )
